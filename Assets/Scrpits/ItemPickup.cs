3f1eeaa4b3b29ac45ac719bac35a48b0e7a46045
S0l4k <lukasz.drobik6@gmail.com>
2025-10-17 01:34:21 +0200

    ulepszenie podnoszenia, drop itd

--- Assets/Scrpits/ItemPickup.cs
+++ Assets/Scrpits/ItemPickup.cs
@@ -4,40 +4,52 @@ using TMPro;
 public class ItemPickup : MonoBehaviour
 {
     [Header("Item Settings")]
-    public string itemName = "Item";           
-    public Transform handPosition;             
-    public Vector3 localPositionOffset;   
-    public Vector3 localRotationOffset;   
-                                          
+    public string itemName = "Item";
+    public Transform handPosition;
+    public Vector3 localPositionOffset;
+    public Vector3 localRotationOffset;
 
     [Header("UI")]
-    public Canvas pickupCanvas;               
-    public TMP_Text pickupText;                
+    public TMP_Text pickupText; // wspólny TextMeshPro (np. na UI Canvasie)
 
     private Camera playerCamera;
     private bool canPickup = false;
+    private bool isHeld = false;
+
+    private static bool playerHasItem = false;     // Czy gracz trzyma coœ?
+    private static ItemPickup currentTarget = null; // Który item pokazuje tekst
 
     void Start()
     {
-        if (pickupCanvas != null)
-            pickupText.gameObject.SetActive(false);
-
         playerCamera = Camera.main;
+
+        if (pickupText == null)
+            pickupText = FindObjectOfType<TMP_Text>();
+
+        if (pickupText != null)
+            pickupText.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        CheckForPickup();
+        if (!isHeld) // tylko jeœli item nie jest trzymany
+            CheckForPickup();
+        else
+            ShowDropText();
 
-        if (canPickup && Input.GetKeyDown(KeyCode.E))
-        {
+        // podnoszenie
+        if (canPickup && !playerHasItem && Input.GetKeyDown(KeyCode.E))
             Pickup();
-        }
+
+        // upuszczanie
+        if (isHeld && Input.GetKeyDown(KeyCode.G))
+            Drop();
     }
 
     void CheckForPickup()
     {
-        if (!playerCamera) return;
+        if (!playerCamera || pickupText == null)
+            return;
 
         Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
         RaycastHit hit;
@@ -45,48 +57,80 @@ public class ItemPickup : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, pickupRange))
         {
-            if (hit.collider.gameObject == gameObject)
+            if (hit.collider.gameObject == gameObject && !playerHasItem)
             {
-                if (pickupCanvas != null)
-                {
-                    pickupText.gameObject.SetActive(true);
-                    pickupText.text = $"Press E to pick up {itemName}";
-                }
+                currentTarget = this;
+                ShowPickupText();
                 canPickup = true;
                 return;
             }
         }
 
-        if (pickupCanvas != null)
-            pickupText.gameObject.SetActive(false);
-
-        canPickup = false;
-    }
-    void Pickup()
-    {
-        if (handPosition != null)
+        if (currentTarget == this)
         {
-            transform.SetParent(handPosition);
-
-            
-            transform.localPosition = localPositionOffset;
-
-            
-            transform.localRotation = Quaternion.Euler(localRotationOffset);
-
-           
-            Rigidbody rb = GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.isKinematic = true;  
-                rb.detectCollisions = false; 
-            }
+            HidePickupText();
+            currentTarget = null;
         }
 
-        if (pickupCanvas != null)
-            pickupCanvas.gameObject.SetActive(false);
-
         canPickup = false;
     }
 
+    void ShowPickupText()
+    {
+        pickupText.gameObject.SetActive(true);
+        pickupText.text = $"Press E to pick up {itemName}";
+    }
+
+    void ShowDropText()
+    {
+        if (pickupText != null && playerHasItem)
+        {
+            pickupText.gameObject.SetActive(true);
+            pickupText.text = $"Press G to drop {itemName}";
+        }
+    }
+
+    void HidePickupText()
+    {
+        if (pickupText != null)
+            pickupText.gameObject.SetActive(false);
+    }
+
+    void Pickup()
+    {
+        transform.SetParent(handPosition);
+        transform.localPosition = localPositionOffset;
+        transform.localRotation = Quaternion.Euler(localRotationOffset);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+            rb.detectCollisions = false;
+        }
+
+        isHeld = true;
+        playerHasItem = true;
+
+        HidePickupText();
+        currentTarget = null;
+        canPickup = false;
+    }
+
+    void Drop()
+    {
+        transform.SetParent(null);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+            rb.detectCollisions = true;
+        }
+
+        isHeld = false;
+        playerHasItem = false;
+
+        HidePickupText();
+    }
 }
